ef9697a580b93d60a93c66726ee01b2dae091e0c
MusnudBillahFaysal <138293528+MusnudBillahFaysal@users.noreply.github.com>
2023-07-06 17:02:14 +0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# shopping-cart
+ new shopping cart
---
+++ src/App.js
@@ -0,0 +1,25 @@
+//feature 1
+//import { useState } from 'react';
+//import reactLogo from './assets/react.svg';
+import './App.css';
+//import data from './data.json';
+
+function App() {
+  return (
+    <div className="grid-container">
+      <header>
+        <a href="/">React shopping cart test</a>
+      </header>
+      <main>
+        <div className="content">
+          <div className="main">Products</div>
+
+          <div className="sidebar">Cart Items</div>
+        </div>
+      </main>
+      <footer>All rights reserved</footer>
+    </div>
+  );
+}
+
+export default App;
