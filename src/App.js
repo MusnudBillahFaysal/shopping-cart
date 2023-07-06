96f7a869670e2a9c7228153aeb6bb54519043b21
MusnudBillahFaysal <138293528+MusnudBillahFaysal@users.noreply.github.com>
2023-07-06 20:28:54 +0600

    first commit
    here i attach feature1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-//feature 1
-//import { useState } from 'react';
-//import reactLogo from './assets/react.svg';
-import './App.css';
-//import data from './data.json';
+// feature 1
+import React from 'react';
+import data from './data.json';
+import Products from './components/Products';
 
-function App() {
-  return (
-    <div className="grid-container">
-      <header>
-        <a href="/">React shopping cart test</a>
-      </header>
-      <main>
-        <div className="content">
-          <div className="main">Products</div>
-
-          <div className="sidebar">Cart Items</div>
-        </div>
-      </main>
-      <footer>All rights reserved</footer>
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      products: data.products,
+      size: '',
+      sort: '',
+    };
+  }
+  render() {
+    return (
+      <div className="grid-container">
+        <header>
+          <a href="/">React Shopping Cart</a>
+        </header>
+        <main>
+          <div className="content">
+            <div className="main">
+              <Products products={this.state.products}></Products>
+            </div>
+            <div className="sidebar">Cart Items</div>
+          </div>
+        </main>
+        <footer>All right is reserved.</footer>
+      </div>
+    );
+  }
 }
 
 export default App;
