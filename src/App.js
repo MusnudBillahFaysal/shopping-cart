51e5393f60325d3d0f4ef8683427a3d744426443
MusnudBillahFaysal <138293528+MusnudBillahFaysal@users.noreply.github.com>
2023-07-07 22:28:57 +0600

    cart component
    i add here cart component here
    also removing from product from cart

--- src/App.js
+++ src/App.js
@@ -3,16 +3,41 @@ import React from 'react';
 import data from './data.json';
 import Products from './components/Products';
 import Filter from './components/Filter';
+import Cart from './components/Cart';
 
 class App extends React.Component {
   constructor() {
     super();
     this.state = {
       products: data.products,
+      cartItems: [],
       size: '',
       sort: '',
     };
   }
+
+  removeFromCart = (product) => {
+    const cartItems = this.state.cartItems.slice();
+    this.setState({
+      cartItems: cartItems.filter((X) => X._id !== product._id),
+    });
+  };
+
+  addTocart = (product) => {
+    const cartItems = this.state.cartItems.slice();
+    let alreadyInCart = false;
+    cartItems.forEach((item) => {
+      if (item._id === product._id) {
+        item.count++;
+        alreadyInCart = true;
+      }
+    });
+
+    if (!alreadyInCart) {
+      cartItems.push({ ...product, count: 1 });
+    }
+    this.setState({ cartItems });
+  };
 
   sortProducts = (event) => {
     const sort = event.target.value;
@@ -66,9 +91,17 @@ class App extends React.Component {
                 filterProducts={this.filterProducts}
                 sortProducts={this.sortProducts}
               ></Filter>
-              <Products products={this.state.products}></Products>
+              <Products
+                products={this.state.products}
+                addTocart={this.addTocart}
+              ></Products>
             </div>
-            <div className="sidebar">Cart Items</div>
+            <div className="sidebar">
+              <Cart
+                cartItems={this.state.cartItems}
+                removeFromCart={this.removeFromCart}
+              />
+            </div>
           </div>
         </main>
         <footer>All right is reserved.</footer>
---
+++ src/components/Cart.js
@@ -0,0 +1,65 @@
+import React, { Component } from 'react';
+import formatCurrency from '../util';
+
+export default class Cart extends Component {
+  render() {
+    const { cartItems } = this.props;
+    return (
+      <div>
+        {cartItems.length === 0 ? (
+          <div className="cart cart-header">Cart is empty</div>
+        ) : (
+          <div className="cart cart-header">
+            You have {cartItems.length} in the cart{' '}
+          </div>
+        )}
+        <div>
+          <div>
+            <div className="cart">
+              <ul className="cart-items">
+                {cartItems.map((item) => (
+                  <li key={item._id}>
+                    <div>
+                      <img src={item.image} alt={item.title}></img>
+                    </div>
+                    <div>
+                      <div>{item.title}</div>
+                      <div className="price">
+                        {formatCurrency(item.price)} x {item.count}{' '}
+                      </div>
+                      <div className="right">
+                        <button
+                          className="button"
+                          onClick={() => this.props.removeFromCart(item)}
+                        >
+                          Remove
+                        </button>
+                      </div>
+                    </div>
+                  </li>
+                ))}
+              </ul>
+            </div>
+            {cartItems.length !== 0 && (
+              <div className="cart">
+                <div className="total">
+                  <div>
+                    <b>
+                      Total:{' '}
+                      {formatCurrency(
+                        cartItems.reduce((a, c) => a + c.price * c.count, 0)
+                      )}
+                    </b>
+                  </div>
+                  <button className="button primary">
+                    <b>Proceed</b>
+                  </button>
+                </div>
+              </div>
+            )}
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
